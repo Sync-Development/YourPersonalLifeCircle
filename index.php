09a84ceff9f28a78f0334f573222ffbdb0329f87
Joram Baak <31468700+SyncDeveloperJ@users.noreply.github.com>
2018-01-16 13:35:22 +0100

    Slider YourLifeCircle gefixed
    De slider van YourLifeCircle heeft nu 3 images en laat niet meer 6 images zien waarvan 3 lege. Ook zijn de secondes nu wat langer waardoor elke slider er wat langer op staat.

--- index.php
+++ index.php
@@ -43,9 +43,6 @@
             <li><span>Image 01</span></li>
             <li><span>Image 02</span></li>
             <li><span>Image 03</span></li>
-            <li><span>Image 04</span></li>
-            <li><span>Image 05</span></li>
-            <li><span>Image 06</span></li>
         </ul>
         <div class="container">
         <div class="navbar navbar-default navbar-static-top">
